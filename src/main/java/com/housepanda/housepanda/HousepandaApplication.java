b2cc0acca8de455c7d65337ad9bc7b1754118019
John Castro <johhncaastro@gmail.com>
2024-06-27 07:34:55 -0500

    inital commit

---
+++ src/main/java/com/housepanda/housepanda/HousepandaApplication.java
@@ -0,0 +1,13 @@
+package com.housepanda.housepanda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HousepandaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HousepandaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/housepanda/housepanda/HousepandaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.housepanda.housepanda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HousepandaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
